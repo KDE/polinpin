7160cb37e6e29442a430ce28f7037fd5cd622d20
Jan Blackquill <uhhadd@gmail.com>
2022-05-31 22:53:15 -0400

    feat: port

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -4,6 +4,7 @@ import Vapor
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
+app.http.server.configuration.port = 25727
 defer { app.shutdown() }
 try configure(app)
 try app.run()
