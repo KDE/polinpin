f23693f0039b9fa8ae4993b4bc3f26d03fa6c334
Jan Blackquill <uhhadd@gmail.com>
2022-06-23 20:24:21 -0400

    feat: set port

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,10 +1,15 @@
 import App
 import Vapor
 
+struct AppConfiguration: Codable {
+    var port: Int = 25727
+}
+
+let appConfig = try! JSONDecoder().decode(AppConfiguration.self, from: try! String(contentsOfFile: "Polinpin Configuration.json").data(using: .utf8)!)
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
-app.http.server.configuration.port = 25727
+app.http.server.configuration.port = appConfig.port
 defer { app.shutdown() }
 try configure(app)
 try app.run()
